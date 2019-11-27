5b62b3b9553470148f69d5f4a61f7dd2d7142956
vearvip <vear.vip@qq.com>
2019-11-27 22:58:24 +0800

    van4er

--- src/pages/Friend/Friend.jsx
+++ src/pages/Friend/Friend.jsx
@@ -15,14 +15,11 @@ const Friend = props => {
   navSelectedItemKey !== 'friend' && setNavSelectedItemKeyAction('friend')
   return (
     <div className="friend">
-      <Above imgUrl={'https://s2.ax1x.com/2019/11/26/Mzb75Q.jpg'} />
+      <Above imgUrl={'https://s2.ax1x.com/2019/11/27/QC4sXV.jpg'} />
       <div className="friend-main">
-        <p>friend</p>
         <span role="img" aria-label="施工中" style={{
           color: 'red'
         }}>施工中🚧</span>
-        <br />
-        <p>联系方式：vear.vip@qq.com</p>
       </div>
     </div>
   )
