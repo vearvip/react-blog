0b93e7d3a26fda7167834083e0711697a95a9afe
vearvip <vear.vip@qq.com>
2020-02-09 00:50:28 +0800

    可还行

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './index.less'
 import { DatePicker } from 'antd'
+import Nav from '../components/Nav'
 
 function HomePage() {
   return (
     <div>
+      <Nav />
       <DatePicker />
+      <img src="/favicon.ico"/>
     </div>
   )
 }
