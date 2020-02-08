9e56bbf1446808dd47b0e5bcfaad7fe38cfe3532
vearvip <vear.vip@qq.com>
2020-02-09 01:00:22 +0800

    调整全局主题色

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withLess = require('@zeit/next-less')
 module.exports = withLess({
   lessLoaderOptions: {
-    javascriptEnabled: true
+    javascriptEnabled: true,
+    modifyVars: {
+      '@primary-color': '#ff7044',
+      '@link-color': 'rgb(123, 123, 248)'
+    }
   }
 })
